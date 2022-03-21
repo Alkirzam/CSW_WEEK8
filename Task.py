68f114d954c38ca8005a06c56ba6791a3c8323f5
Ibbyys <97961647+Ibbyys@users.noreply.github.com>
2022-03-21 21:03:27 +0000

    Ibby

--- Task.py
+++ Task.py
@@ -6,18 +6,20 @@ import matplotlib.pyplot as plt
 pd.set_option('display.max_columns', None)
 pd.options.mode.chained_assignment = None
 
-movies_df = pd.read_csv('data.csv')
-movies_df = movies_df[movies_df.original_release_date.notnull()]
-movies_df['original_release_date'] = pd.to_datetime(movies_df['original_release_date'])
-movies_df['movie_year'] = movies_df['original_release_date'].apply(lambda x: x.year)
+reviews_df = pd.read_csv('reviews.csv')
 
-sns.set(style="white")
+
+reviews_df = reviews_df[reviews_df.review_date.notnull()]
+reviews_df['review_date'] = pd.to_datetime(reviews_df['review_date'])
+reviews_df['review_year'] = reviews_df['review_date'].apply(lambda x: x.year)
+reviews_df = reviews_df[reviews_df.review_year.astype(int) >= 2000]
 
 plt.figure(figsize=(15,10))
-plt.title('Movies by the year', size=20)
-sns.distplot(movies_df.movie_year, kde=False)
-plt.ylabel('Number of movies', size=15)
-plt.xlabel('Year of release',size=15)
-plt.axis([1920, 2019, 0, 1750])
-plt.xticks(np.arange(1920, 2018, step=5),rotation=45, ha='right')
+plt.title('Reviews by the year', size=20)
+sns.distplot(reviews_df.review_year, bins=20, kde=False)
+plt.ylabel('Number of critic reviews', size=15)
+plt.xlabel('Year of review posted',size=15)
+plt.axis([2000, 2019, 0, 75000])
+plt.xticks(np.arange(2000, 2019, step=1),rotation=45, ha='right')
+plt.show()
 plt.show()
