58c13e7697b0a928c0b4bdfacb1d3eea49e5d580
alialkirzam@hotmail.com <alialkirzam@hotmail.com>
2022-03-23 13:04:14 +0000

    A

--- test.py
+++ test.py
@@ -1,24 +1,19 @@
 import pandas as pd
-import numpy as np
-from pickle import TRUE
 import seaborn as sns
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 movies_df = pd.read_csv('C:/Users/alialkirzam/OneDrive - Liverpool John Moores University/Documents/GitHub/CSW_WEEK8/CSW_WEEK8-1/rotten_tomatoes_movies.csv')
-movies_df['Studio'] = movies_df['production_company'].str.split(' ').str[0]
+
+movies_df['genre'] = movies_df['genres'].str.split(',').str[0]
+count = movies_df['genre'].value_counts()[:7]
 
 
-count = movies_df['Studio'].value_counts()[:10]
-top_Studio = list(count.index)
 
-movie_Studio_df = movies_df[movies_df['Studio'].isin(top_Studio)]
-
+top_genres = list(count.index)
+movie_genres_df = movies_df[movies_df['genre'].isin(top_genres)]
 plt.figure(figsize=(15, 10))
-
-sns.set_style("whitegrid")
-sns.barplot(x='Studio', y='tomatometer_rating', data=movie_Studio_df)
-plt.xlabel("Studio names",fontsize=10)
-plt.ylabel("TomatoMeter Rating",fontsize=10)
-plt.title("Barplot of TomatoMeter rating per Studio ", fontsize=15)
+sns.boxplot(x='genre', y='audience_rating', data=movie_genres_df)
+plt.xlabel("Genre Name",fontsize=12)
+plt.ylabel("Audience Rating",fontsize=12)
+plt.title("Boxplot of Audience rating per Genre", fontsize=16)
 plt.show()
