13b6f88ad91deb57b5bcf0f4e35c84a031eb43d2
Abubakr <abubakrabdulraheem2003@gmail.com>
2022-03-29 13:57:03 +0100

    practice visulas

---
+++ practice/practice.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+x = [1, 2, 3]
+y = [1, 4, 9]
+plt.plot(x,y)
+plt.show()
---
+++ practice2.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+movie_genres_df= pd.read_csv('C:/Users/hanis/workshop coursework/CSW_ week 8/CSW_WEEK8/rotten_tomatoes_movies (2).csv')
+
+
+
+
+f,ax = plt.subplots(3,1, figsize=(15, 30))
+sns.distplot(movie_genres_df[(movie_genres_df['tomatometer_status'] == 'Certified-Fresh') &
+                             (movie_genres_df['tomatometer_count'] <= 400)].tomatometer_count, ax=ax[0], bins=30)
+ax[0].set_title('TomatoMeter count in Certified Fresh', fontsize=16)
+ax[0].set_xlabel("TomatoMeter Count",fontsize=12)
+ax[0].set_xlim([0,400])
+
+
+sns.distplot(movie_genres_df[(movie_genres_df['tomatometer_status'] == 'Fresh') &
+                             
+                             (movie_genres_df['tomatometer_count'] <= 400)].tomatometer_count, ax=ax[1], bins=30)
+ax[1].set_title('TomatoMeter count in Fresh', fontsize=16)
+ax[1].set_xlabel("TomatoMeter Count",fontsize=12)
+ax[1].set_xlim([0,400])
+
+
+sns.distplot(movie_genres_df[(movie_genres_df['tomatometer_status'] == 'Rotten') &
+                             
+                             
+                             (movie_genres_df['tomatometer_count'] <= 400)].tomatometer_count, ax=ax[2], bins=30)
+ax[2].set_title('TomatoMeter count in Rotten', fontsize=16)
+ax[2].set_xlabel("TomatoMeter Count",fontsize=16)
+ax[2].set_xlim([0,400])
+plt.show()
